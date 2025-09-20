11a1f0b1b3933bcebe7a7cab162986f58361316b
pritamjitmanna <pritamjitmanna@gmail.com>
2025-09-20 21:30:29 +0530

    Experimenting to use node js utility container for running the backend project. Also added the url for using the mongodb container

--- Backend/Database/mongo.js
+++ Backend/Database/mongo.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose")
 
-const mongoURI="mongodb://localhost:27017/task_management_tool?readPreference=primary&directConnection=true&ssl=false"
+// const mongoURI="mongodb://localhost:8081/task_management_tool?readPreference=primary&directConnection=true&ssl=false"
+
+const mongoURI=`mongodb://mongo-db:${process.env.MONGO_PORT}/task_management_tool`          //Docker URI with the same network running 
 
 
 
